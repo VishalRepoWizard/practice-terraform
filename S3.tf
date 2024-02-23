56282d6f4b6ea7d55c59bf57eabe9674334d2bca
Vishal <rajput.vishal.rajesh@gmail.com>
2024-02-23 21:06:24 +0530

    terraform_operations

---
+++ S3.tf
@@ -0,0 +1,12 @@
+# provider "aws" {
+#     region = "ap-south-1"
+# }
+
+# resource "aws_s3_bucket" "Bucket" {
+#   bucket = "uhgdyuwfhwefbwyfgrf"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
---
+++ redhat-server.tf
@@ -0,0 +1,12 @@
+# provider "aws" {
+#     region = "ap-south-1"
+# }
+
+# resource "aws_instance" "Redhat" {
+#     ami                     = "ami-05a5bb48beb785bf1"
+#     instance_type           = "t2.micro"
+
+#     tags = {
+#         Name = "RedHat-server"
+#     }
+# }
